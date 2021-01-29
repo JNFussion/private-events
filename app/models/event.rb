72557695de38dcbc9bc2935b8b47d30b6d249ab2
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-01-29 14:21:19 +0100

    add partial. Explit event into upcoming and past using scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: :User
   has_many :event_attendance, foreign_key: "attended_event_id"
   has_many :attendees, through: :event_attendance, source: :attendees
+
+  scope :past, -> { where("event_date < ?", Time.now) }
+  scope :upcoming, -> { where("event_date > ?", Time.now) }
 end
